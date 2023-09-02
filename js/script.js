dd45b912d2f024e6816dd949fce406ad3ac8c0f8
Hemonta Mondal <86686198+hemontamondal@users.noreply.github.com>
2023-09-02 14:21:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// text change
+function textChange1(){
+	document.getElementById('tex1').innerHTML="Thank you";
+}
+
+function textChange2(){
+	document.getElementById('text2').innerHTML="Thank you";
+}
+
+function textChange3(){
+	document.getElementById('text3').innerHTML="Thank you";
+}
+
+function textChange4(){
+	document.getElementById('text4').innerHTML="Thank you";
+}
+
+function textChange5(){
+	document.getElementById('text5').innerHTML="Thank you";
+}
+
+function textChange6(){
+	document.getElementById('text6').innerHTML="Thank you";
+}
+
+function textChange7(){
+	document.getElementById('text7').innerHTML="Thank you";
+}
+
+function textChange8(){
+	document.getElementById('text8').innerHTML="Thank you";
+}
+
+function textChange9(){
+	document.getElementById('text9').innerHTML="Thank you";
+}
+
+function textChange10(){
+	document.getElementById('text10').innerHTML="Thank you";
+}
+
+function textChange11(){
+	document.getElementById('text11').innerHTML="Thank you";
+}
+
+function textChange(){
+	document.getElementById('text12').innerHTML="Thank you";
+}
